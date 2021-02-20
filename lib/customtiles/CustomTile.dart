defafcd576ce740c3bdbb97689bdebb0d5d7a0c4
Joe Willmott <joe@joewillmott.co.uk>
2021-02-20 19:35:29 +0000

    chats n shit ya know

--- lib/customtiles/CustomTile.dart
+++ lib/customtiles/CustomTile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:uni_roomie/blocs/auth_bloc.dart';
+import 'package:uni_roomie/screens/chats/ViewChatsPage.dart';
 
 class CustomDrawerTile extends StatefulWidget {
   IconData icon;
@@ -21,7 +22,10 @@ class _CustomDrawerTile extends State<CustomDrawerTile> {
       padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
       child: Container(
         decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(color: new Color.fromRGBO(249, 89, 89, 1),))),
+            border: Border(
+                bottom: BorderSide(
+          color: new Color.fromRGBO(249, 89, 89, 1),
+        ))),
         child: InkWell(
           splashColor: new Color.fromRGBO(249, 89, 89, 0.8),
           onTap: widget.onTap,
@@ -69,8 +73,10 @@ class _CustomDrawerState extends State<CustomDrawer> {
         children: <Widget>[
           DrawerHeader(
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: <Color>[new Color.fromRGBO(180, 190, 201, 1), new Color.fromRGBO(69, 93, 122, 1)])),
+                gradient: LinearGradient(colors: <Color>[
+              new Color.fromRGBO(180, 190, 201, 1),
+              new Color.fromRGBO(69, 93, 122, 1)
+            ])),
             child: Container(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -79,7 +85,8 @@ class _CustomDrawerState extends State<CustomDrawer> {
                     elevation: 20,
                     child: Padding(
                       padding: EdgeInsets.all(5.0),
-                      child: Image.asset('assets/logo.png',
+                      child: Image.asset(
+                        'assets/logo.png',
                         height: 100,
                         width: 100,
                       ),
@@ -93,7 +100,19 @@ class _CustomDrawerState extends State<CustomDrawer> {
           CustomDrawerTile(Icons.directions, 'Create a Listing', () => {}),
           CustomDrawerTile(Icons.star, 'View Listings', () => {}),
           CustomDrawerTile(Icons.list, 'Listing Requests', () => {}),
-          CustomDrawerTile(Icons.logout, 'Logout', () => {widget.authBloc.logout()}),
+          CustomDrawerTile(
+              Icons.list,
+              'Chats',
+              () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ViewChatsPage(),
+                      ),
+                    ),
+                  }),
+          CustomDrawerTile(
+              Icons.logout, 'Logout', () => {widget.authBloc.logout()}),
         ],
       ),
     );
