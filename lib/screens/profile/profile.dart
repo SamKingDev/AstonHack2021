6e3a86bba4d6f6f178cfa294fe05836fec097e9a
unknown <emma12812@gmail.com>
2021-02-20 13:54:51 +0000

    Working on profile page layout

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -30,39 +30,127 @@ class _ProfilePageState extends State<ProfilePage> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     var authBloc = Provider.of<AuthBloc>(context, listen: false);
     return Scaffold(
       drawer: CustomDrawer(authBloc),
-      appBar: AppBar(title: Text(''),
-          centerTitle: true),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-
-          ),
-          Container(
-
-          ),
-          Container(
-
-          ),
-          Container(
-
-          ),
-          Container(
-
-          ),
-          Container(
-
-          ),
-          Container(
-
-          ),
-        ]),
+      appBar: AppBar(
+        title: Text('Your Profile'),
+        centerTitle: true,
+        backgroundColor: new Color.fromRGBO(69, 93, 122, 1),
       ),
+      body: SingleChildScrollView(
+          child: Column(children: [
+        Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: new Color.fromRGBO(180, 190, 201, 1),
+              ),
+              child: Container(
+                child: Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/avatar4.png'),
+                    radius: 40.0,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          'Change Avatar',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(
+                          Icons.camera_alt,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: new Color.fromRGBO(180, 190, 201, 1),// set border width
+              borderRadius: BorderRadius.all(
+                  Radius.circular(10.0)), // set rounded corner radius
+              boxShadow: [BoxShadow(blurRadius: 10,color: Colors.black,offset: Offset(1,3))]),
+          child: Column(
+            children: [
+              Container(
+                child: CustomProfileTile(Icons.account_box, 'Name', 'Emma Sutton'),
+              ),
+              Container(
+                child: CustomProfileTile(
+                    Icons.alternate_email, 'Email', 'Test@gmail.com'),
+              ),
+              Container(
+                child: CustomProfileTile(Icons.person, 'Gender', 'Female'),
+              ),
+              Container(
+                child: CustomProfileTile(Icons.grade, 'Age', '21'),
+              ),
+              Container(
+                child: CustomProfileTile(
+                    Icons.school, 'University', 'Southampton Solent'),
+              ),
+              Container(
+                child: CustomProfileTile(
+                    Icons.bookmark, 'Course', 'Software Engineering'),
+              ),
+              Container(
+                child: CustomProfileTile(Icons.trending_up, 'Year Of Study', '3'),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0)),
+                  onPressed: () {},
+                  color: new Color.fromRGBO(249, 89, 89, 1),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Edit Profile',
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        Icon(
+                          Icons.edit,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(),
+            ],
+          ),
+        ),
+      ])),
     );
   }
 }
