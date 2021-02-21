dce280be5a315d7cf7a7338681518930e5f1ad7b
Sam King <smadger@live.co.uk>
2021-02-21 01:04:12 +0000

    Images working fully on listings!

--- lib/screens/viewListings/singleListing.dart
+++ lib/screens/viewListings/singleListing.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:uni_roomie/objects/listing.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -24,8 +25,40 @@ class _SingleListingPageState extends State<SingleListingPage> {
             }));
   }
 
+  int _current = 0;
   @override
   Widget build(BuildContext context) {
+    final List<Widget> imageSliders = widget.listing.photoURLs.map((item) => Container(
+      child: Container(
+        margin: EdgeInsets.all(5.0),
+        child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+            child: Stack(
+              children: <Widget>[
+                Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                Positioned(
+                  bottom: 0.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(200, 0, 0, 0),
+                          Color.fromARGB(0, 0, 0, 0)
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  ),
+                ),
+              ],
+            )
+        ),
+      ),
+    )).toList();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: new Color.fromRGBO(69, 93, 122, 1),
@@ -47,8 +80,40 @@ class _SingleListingPageState extends State<SingleListingPage> {
               ]),
           child: Column(children: [
             Container(
-                //list of photos
-
+                  child: Column(
+                      children: [
+                        CarouselSlider(
+                          items: imageSliders,
+                          options: CarouselOptions(
+                              autoPlay: true,
+                              enlargeCenterPage: true,
+                              aspectRatio: 2.0,
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  _current = index;
+                                });
+                              }
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: widget.listing.photoURLs.map((url) {
+                            int index = widget.listing.photoURLs.indexOf(url);
+                            return Container(
+                              width: 8.0,
+                              height: 8.0,
+                              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _current == index
+                                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                                    : Color.fromRGBO(0, 0, 0, 0.4),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ]
+                  ),
                 ),
             Container(
               child: ListingInformationTile(
